36a2119d34545f35e32ffe897e2e9bb08d2bffd2
Wunkolo <Wunkolo@gmail.com>
2024-08-17 16:24:33 -0700

    Fix benchmark structure type resolution
    `result_of` is removed in favor of `std::invoke_result_t`

--- tests/Bench.hpp
+++ tests/Bench.hpp
@@ -1,28 +1,26 @@
 #pragma once
-#include <tuple>
 #include <chrono>
+#include <tuple>
 
-template< typename TimeT = std::chrono::nanoseconds >
+template<typename TimeT = std::chrono::nanoseconds>
 struct Bench
 {
 
 	// Returns a tuple of (Time, Function return value)
-	template< typename FunctionT, typename ...ArgsT >
-	static std::tuple<
-	TimeT,
-	typename std::result_of<FunctionT(ArgsT...)>::type
-	> BenchResult( FunctionT&& Func, ArgsT&&... Arguments )
+	template<
+		typename FunctionT, typename... ArgsT,
+		typename ResultT = std::invoke_result_t<FunctionT, ArgsT...>>
+	static std::tuple<TimeT, ResultT>
+		BenchResult(FunctionT&& Func, ArgsT&&... Arguments)
 	{
 		const auto Start = std::chrono::high_resolution_clock::now();
 
-		typename std::result_of<FunctionT(ArgsT...)>::type ReturnValue
-		= std::forward<FunctionT>(Func)(std::forward<ArgsT>(Arguments)...);
+		ResultT ReturnValue
+			= std::forward<FunctionT>(Func)(std::forward<ArgsT>(Arguments)...);
+
 		const auto Stop = std::chrono::high_resolution_clock::now();
 
-		return std::make_tuple<
-		TimeT,
-		typename std::result_of<FunctionT(ArgsT...)>::type
-		>(
+		return std::make_tuple<TimeT, ResultT>(
 			std::chrono::duration_cast<TimeT>(Stop - Start),
 			std::move(ReturnValue)
 		);
