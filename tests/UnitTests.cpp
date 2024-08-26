2b02c72cc5e53e26a45969a024d1aa2eeb7c471c
Wunkolo <Wunkolo@gmail.com>
2024-08-25 21:08:34 -0700

    Add overflow unit tests
    Unit tests to help aid in detecting overflow issues.  Utilizes an
    `std::vector` rather than `std::array` to avoid overflowing the stack.

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cstdint>
+#include <vector>
 
 #include <catch2/catch_test_macros.hpp>
 
@@ -77,11 +78,31 @@ TEST_CASE("512 constant", "[Average]")
 	REQUIRE(qAverageColorRGBA8(Data.data(), Data.size()) == 0xAA'BB'CC'DD);
 }
 
-TEST_CASE("1000 constant", "[Average]")
+TEST_CASE("1'000 constant", "[Average]")
 {
 	std::array<std::uint32_t, 1000> Data;
 	Data.fill(0xAA'BB'CC'DD);
 
 	REQUIRE(AverageColorRGBA8(Data.data(), Data.size()) == 0xAA'BB'CC'DD);
 	REQUIRE(qAverageColorRGBA8(Data.data(), Data.size()) == 0xAA'BB'CC'DD);
+}
+
+TEST_CASE("100'000'000 constant", "[Average]")
+{
+	std::vector<std::uint32_t> Data(100'000'000, 0xFF'FF'FF'FF);
+
+	REQUIRE(AverageColorRGBA8(Data.data(), Data.size()) == 0xFF'FF'FF'FF);
+	REQUIRE(qAverageColorRGBA8(Data.data(), Data.size()) == 0xFF'FF'FF'FF);
+}
+
+TEST_CASE("32-sum overflow", "[Average]")
+{
+	// In the worst case, where all the bytes are just 0xFF being summed
+	// into a 32-bit accumulator. The 32-bit accumulator wculd overflow
+	// after-( (0xFFFFFFFF / ( 0xFF ) ) = >>> 0x1010101 iterations <<<
+	const std::size_t          LocalSumMaxIter = (0xFFFFFFFF / 0xFF);
+	std::vector<std::uint32_t> Data(LocalSumMaxIter * 2, 0xFF'FF'FF'FF);
+
+	REQUIRE(AverageColorRGBA8(Data.data(), Data.size()) == 0xFF'FF'FF'FF);
+	REQUIRE(qAverageColorRGBA8(Data.data(), Data.size()) == 0xFF'FF'FF'FF);
 }
